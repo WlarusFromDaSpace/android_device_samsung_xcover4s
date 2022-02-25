986180bc1676f6a63ff6306ec0287467c54f430a
WlarusFromDaSpace <walrusfromspace@tutanota.com>
2022-02-25 19:29:19 +0200

    Update

--- omni_xcover4s.mk
+++ omni_xcover4s.mk
@@ -1,12 +1,12 @@
-PRODUCT_RELEASE_NAME := Xcover4s
+PRODUCT_RELEASE_NAME := xcover4s
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_DEVICE := Xcover4s
-PRODUCT_NAME := omni_Xcover4s
+PRODUCT_DEVICE := xcover4s
+PRODUCT_NAME := omni_xcover4s
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := Xcover4s
+PRODUCT_MODEL := xcover4s
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_RELEASE_NAME := Galaxy Xcover 4s
+PRODUCT_RELEASE_NAME := Galaxy xcover 4s
